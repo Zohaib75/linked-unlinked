25d13a0d777227a3306ceedcfce4e78de361ec5d
zohaib <iamzohaibsohail@gmail.com>
2020-09-07 17:04:27 +0500

    function unlinkedUSers added

--- linked.js
+++ linked.js
@@ -1,1 +1,14 @@
 // find all the users that have not been linked
+
+const {allUsers, linked} = require('./users')
+
+function unlinkedUsers(allUsers, linked) {
+    return allUsers.filter((user) => {
+        return !linked.find(linkedUser => {
+            return linkedUser.id === user.id;
+        })
+    })
+};
+
+let unlinkedUsersArray = unlinkedUsers(allUsers, linked);
+console.log(unlinkedUsersArray)
